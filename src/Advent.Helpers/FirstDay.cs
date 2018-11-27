dd174f18c479f1074f67cd81ae2fa199c1b175b1
uselessfulness <mattm@firemail.cc>
2018-11-27 15:03:25 +0000

    Move character to int conversion to helper class for now

---
+++ src/Advent.Helpers/FirstDay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advent.Helpers
+{
+    public static class Conversion
+    {
+        public static int CharToInt(char c)
+        {
+            return (int)char.GetNumericValue(c);
+        }
+    }
+}
