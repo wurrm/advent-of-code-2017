f5d7515abfecb54c188432cf06c6c2db35280c57
uselessfulness <mattm@firemail.cc>
2018-11-27 13:31:38 +0000

    Solve the 1st puzzle

---
+++ First/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int output = RecurseReverseCaptcha(args[0], CharToInt(args[0][args[0].Length - 1]), 0);
+            Console.WriteLine(output);
+        }
+
+        static int CharToInt(char c)
+        {
+            return (int)char.GetNumericValue(c);
+        }
+
+        // Tail recursion because reverse sounds like recurse
+        static int RecurseReverseCaptcha(string input, int prevDigit, int repeatSum)
+        {
+            int currDigit = CharToInt(input[0]);
+
+            if (input.Length == 1)
+            {
+                return repeatSum;
+            }
+            else if (currDigit == prevDigit)
+            {
+                repeatSum += currDigit;
+            }
+
+            return RecurseReverseCaptcha(input.Substring(1, input.Length - 1), currDigit, repeatSum);
+        }
+    }
+}
