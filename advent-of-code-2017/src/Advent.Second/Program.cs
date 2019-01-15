e99d5b9b26015ba674604028932f01d77f30a6af
uselessfulness <mattm@firemail.cc>
2019-01-15 21:18:29 +0000

    Add AoC 2017 Day 2 Part 1 Solution

---
+++ advent-of-code-2017/src/Advent.Second/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent.Second
+{
+    class Program
+    {
+        static void Main()
+        {
+            StreamReader f = new StreamReader("input.txt");
+
+            Console.WriteLine(Checksum(f));
+        }
+
+        static int Checksum(StreamReader f)
+        {
+            string row;
+            List<int> row_checksums = new List<int>();
+
+            while ((row = f.ReadLine()) != null)
+            {
+                int[] pRow = Array.ConvertAll<string, int>(Regex.Split(row, @"\s+"), int.Parse);
+                row_checksums.Add(pRow.Max() - pRow.Min());
+            }
+
+            return row_checksums.Sum();
+        }
+    }
+}
