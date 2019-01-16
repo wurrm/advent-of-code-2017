74be158d4d99d645b7046fa83ac504c5729ef0ad
uselessfulness <mattm@firemail.cc>
2019-01-16 21:29:17 +0000

    Add AoC Day 2 Part 2 solution

--- advent-of-code-2017/src/Advent.Second/Program.cs
+++ advent-of-code-2017/src/Advent.Second/Program.cs
@@ -8,8 +8,29 @@ namespace Advent.Second
 {
     class Program
     {
-        static void Main()
+        public delegate int CalculateRow(int[] row);
+
+        static int Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Please specify the number of the part you want to solve (1 or 2).");
+                return 1;
+            }
+
+            CalculateRow fn;
+            switch (args[0])
+            {
+                case "1":
+                    fn = CalculateRowChecksum;
+                    break;
+                case "2":
+                    fn = CalculateRowDivisor;
+                    break;
+                default:
+                    return 1;
+            }
+
             StreamReader f = new StreamReader("input.txt");
 
             string row;
@@ -18,15 +39,34 @@ namespace Advent.Second
             while ((row = f.ReadLine()) != null)
             {
                 int[] pRow = Array.ConvertAll<string, int>(Regex.Split(row, @"\s+"), int.Parse);
-                row_checksums.Add(CalculateRowChecksum(pRow));
+                row_checksums.Add(fn(pRow));
             }
 
             Console.WriteLine(row_checksums.Sum());
+            return 0;
         }
 
         static int CalculateRowChecksum(int[] row)
         {
             return (row.Max() - row.Min());
+        }
+
+        static int CalculateRowDivisor(int[] row)
+        {
+            int remainder;
+            for (int i = 0; i < row.Length; i++)
+            {
+                for (int j = i + 1; j < row.Length; j++)
+                {
+                    remainder = (row[i] < row[j]) ? row[j] % row[i] : row[i] % row[j];
+                    if (row[i] % row[j] == 0)
+                        return row[i] / row[j];
+                    else if (row[j] % row[i] == 0)
+                        return row[j] / row[i];
+                }
+            }
+
+            return 0;
         }
     }
 }
