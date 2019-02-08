97031f648ed955872273aa5ca34992d0ba38d28d
uselessfulness <mattm@firemail.cc>
2019-02-08 22:00:25 +0000

    Add an interesting solution from the book

--- programming-interviews-exposed-questions/chapter-3/remove_dupes.py
+++ programming-interviews-exposed-questions/chapter-3/remove_dupes.py
@@ -5,6 +5,9 @@ given_list = [int(x) for x in sys.argv[1:]]
 new_list = []
 found = []
 
+# A cool and fast solution from the book: list(set(given_list))
+# Provided that they're okay with the list not being in its original order
+
 for x in given_list:
     if x not in found:
         found.append(x)
