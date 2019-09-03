6c9ed1d5e32f6b9a0624c286e31023c6ac6c3c9c
wurrm <wurrm@pm.me>
2019-09-03 16:00:47 +0100

    If it's gonna suck anyway, at least make it consistent

--- programming-interviews-exposed-questions/chapter-3/01_fizzbuzz.c
+++ programming-interviews-exposed-questions/chapter-3/01_fizzbuzz.c
@@ -6,13 +6,21 @@ int main(int argc, char** argv)
     int max = atoi(argv[1]);
     for (int i = 1; i <= max; i++)
     {
-	if (i % 3 == 0 && i % 5 == 0)
-	    printf("FizzBuzz\n");
-	else if (i % 3 == 0)
-	    printf("Fizz\n");
-	else if (i % 5 == 0)
-	    printf("Buzz\n");
-	else
-	    printf("%i\n", i);
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else
+        {
+            printf("%i\n", i);
+        }
     }
 }
